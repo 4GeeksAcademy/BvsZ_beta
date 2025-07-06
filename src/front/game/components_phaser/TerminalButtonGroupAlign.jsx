aec1adf85adfede54f9af9eed63ae1fc0910f4be
Edwin González <130111056+edslogia@users.noreply.github.com>
2025-07-06 23:45:57 +0000

    se ajusta componente align

--- src/front/game/components_phaser/TerminalButtonGroupAlign.jsx
+++ src/front/game/components_phaser/TerminalButtonGroupAlign.jsx
@@ -3,17 +3,17 @@ import TerminalButton from './TerminalButton';
 import './TerminalButtonGroup.css';
 
 const TerminalButtonGroupAlign = () => (
-  <div className='container d-inline mt-4'>
-    <div className="col-auto">
-      <TerminalButton label="align-items-start" onClick={() => {}} />
+    <div className='container d-inline mt-4'>
+        <div className="col-auto">
+            <TerminalButton label="align-items-start" onClick={() => { }} />
+        </div>
+        <div className="col-auto">
+            <TerminalButton label="align-items-center" onClick={() => { }} />
+        </div>
+        <div className="col-auto">
+            <TerminalButton label="align-items-end" onClick={() => { }} />
+        </div>
     </div>
-    <div className="col-auto">
-      <TerminalButton label="align-items-center" onClick={() => {}} />
-    </div>
-    <div className="col-auto">
-      <TerminalButton label="align-items-end" onClick={() => {}} />
-    </div>
-  </div>
 );
 
 export default TerminalButtonGroupAlign;
