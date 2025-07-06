46c800e28aef41cc591a07439cea82c922a513ff
Edwin González <130111056+edslogia@users.noreply.github.com>
2025-07-06 23:55:23 +0000

    se crea botón para pasar a scena game

--- src/front/game/scenes/MainMenu.js
+++ src/front/game/scenes/MainMenu.js
@@ -1,35 +1,53 @@
-import { EventBus } from '../EventBus';
-import { Scene } from 'phaser';
-import { FONT_VT323 } from '../config/fonts';
+import { EventBus } from "../EventBus";
+import { Scene } from "phaser";
+import { FONT_VT323 } from "../config/fonts";
 
 export class MainMenu extends Scene {
-    logoTween;
+  logoTween;
 
-    constructor() {
-        super('MainMenu');
-    }
+  constructor() {
+    super("MainMenu");
+  }
 
-    create() {
+  create() {
+    const bg = this.add.image(0, 0, "background").setOrigin(0);
+    bg.displayWidth = this.sys.game.config.width;
+    bg.displayHeight = this.sys.game.config.height;
 
+    this.add.text(700, 550, "4Geeks", FONT_VT323).setDepth(100).setOrigin(0.5);
 
-        const bg = this.add.image(0, 0, 'background').setOrigin(0);
-        bg.displayWidth = this.sys.game.config.width;
-        bg.displayHeight = this.sys.game.config.height;
+    const user_obj = this.registry.get("user");
+    const user = user_obj?.username || "Invitado";
 
-        this.add.text(700, 550, '4Geeks', FONT_VT323).setDepth(100).setOrigin(0.5);
+    this.add
+      .text(384, 30, `welcome ${user}`, FONT_VT323)
+      .setDepth(100)
+      .setOrigin(0.5);
 
-        const user_obj = this.registry.get('user');
-        const user = user_obj?.username || 'Invitado';
+    // Battle button with border
+    const buttonBg = this.add
+      .rectangle(384, 80, 120, 40, 0x000000)
+      .setStrokeStyle(2, 0xffffff)
+      .setDepth(99)
+      .setInteractive()
+      .on("pointerdown", () => this.changeScene())
+      .on("pointerover", () => {
+        battleButton.setTint(0x6600cc);
+      })
+      .on("pointerout", () => {
+        battleButton.clearTint();
+        buttonBg.setFillStyle(0x000000);
+      });
 
-        this.add.text(384, 50, `Bienvenido ${user}`, FONT_VT323).setDepth(100).setOrigin(0.5);
+    const battleButton = this.add
+      .text(384, 80, "Battle!", FONT_VT323)
+      .setDepth(100)
+      .setOrigin(0.5);
 
-        EventBus.emit('current-scene-ready', this);
+    EventBus.emit("current-scene-ready", this);
+  }
 
-    }
-
-    changeScene() {
-        this.scene.start('Game');
-    }
-
-
+  changeScene() {
+    this.scene.start("Game");
+  }
 }
