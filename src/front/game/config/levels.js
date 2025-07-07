c7803f8d73c05c844472a79406a02454795f2b8c
Edwin González <130111056+edslogia@users.noreply.github.com>
2025-07-07 01:29:54 +0000

    se ajustan columnas inciales

--- src/front/game/config/levels.js
+++ src/front/game/config/levels.js
@@ -5,11 +5,11 @@ export const levels = [
   {
     turretHealth: 50,
     turretsCount: 4,
-    turretsCols: [2, 3, 4, 5],
+    turretsCols: [5, 6, 7, 8],
     gridCols: 12,
     serverCols: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
     serverHealth: 50,
-    zombieVelocityY: -50, //Negativo
+    zombieVelocityY: -20, //Negativo
     zombieHealth: 30,
     zombieDamage: 10,
     bulletVelocityY: 100,
