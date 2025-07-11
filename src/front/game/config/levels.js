9bf319bc45a46d6e34181e1c38717123a4e06fd1
Edwin González <130111056+edslogia@users.noreply.github.com>
2025-07-11 16:09:02 +0000

    se agrega nivel 3

--- src/front/game/config/levels.js
+++ src/front/game/config/levels.js
@@ -32,4 +32,19 @@ export const levels = [
     zombiesPerLevel: 2,
     maxZombiesOnScreen: 10
   },
+  {
+    turretHealth: 50,
+    turretsCount: 4,
+    turretsCols: [5, 6, 7, 8],
+    gridCols: 12,
+    serverCols: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    serverHealth: 50,
+    zombieVelocityY: -80, //Negativo
+    zombieHealth: 30,
+    zombieDamage: 10,
+    bulletVelocityY: 110,
+    bulletDamage: 10,
+    zombiesPerLevel: 3,
+    maxZombiesOnScreen: 10
+  },
 ];
