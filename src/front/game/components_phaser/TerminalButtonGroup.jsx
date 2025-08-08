f83f4424c6ce1ff0d43748b45ed255cbf5e3cdd1
Edwin González <130111056+edslogia@users.noreply.github.com>
2025-08-08 16:58:26 +0000

    ajustes de linea en controles de mouse

--- src/front/game/components_phaser/TerminalButtonGroup.jsx
+++ src/front/game/components_phaser/TerminalButtonGroup.jsx
@@ -92,7 +92,6 @@ const TerminalButtonGroup = ({ levels }) => {
 
   return (
     <div className='container d-inline m-0'>
-      {/* Mostrar las clases seleccionadas actualmente o mensaje instructivo */}
       <div className="active-classes-container mb-1 pt-1 pb-1">
         Active classes: <br />
         <span className="active-classes m-0">
@@ -100,11 +99,11 @@ const TerminalButtonGroup = ({ levels }) => {
             <>
               {/* Primera línea: clases justify */}
               <div>
-                {selectedClasses.filter(cls => cls.startsWith('justify-content-')).join(' ') || 'No justify classes selected'}
+                {selectedClasses.filter(cls => cls.startsWith('justify-content-')).join(' ') }
               </div>
               {/* Segunda línea: clases offset */}
               <div>
-                {selectedClasses.filter(cls => cls.startsWith('offset-')).join(' ') || 'No offset classes selected'}
+                {selectedClasses.filter(cls => cls.startsWith('offset-')).join(' ')}
               </div>
             </>
           ) : (
